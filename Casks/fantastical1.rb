9a284d033759281e22dbfa47d609fd6a53efa1d9
Varun Yellina <varun@yellina.io>
2016-01-18 12:29:47 +0530

    Fix syntax fantastical1
    Copied unnecessary text from the original delete commit of this cask from homebrew/versions

--- Casks/fantastical1.rb
+++ Casks/fantastical1.rb
@@ -1,17 +1,17 @@
 cask 'fantastical1' do
- -  version '1.3.19'
- -  sha256 'a0c8f05072c686aeedd79640ceb887a2b07d6c1f6378b88e53ecdbcd2cb26eec'
- -  
- -  url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
- -  name 'Fantastical'
- -  homepage 'https://flexibits.com/fantastical'
- -  license :freemium
- -  
- -  app 'Fantastical.app'
- -  
- -  postflight do
- -    suppress_move_to_applications
- -  end
- -  
- -  zap :delete => '~/Library/Preferences/com.flexibits.fantastical.plist'
- -end
+  version '1.3.19'
+  sha256 'a0c8f05072c686aeedd79640ceb887a2b07d6c1f6378b88e53ecdbcd2cb26eec'
+
+  url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
+  name 'Fantastical'
+  homepage 'https://flexibits.com/fantastical'
+  license :freemium
+
+  app 'Fantastical.app'
+
+  postflight do
+    suppress_move_to_applications
+  end
+
+  zap delete: '~/Library/Preferences/com.flexibits.fantastical.plist'
+end
