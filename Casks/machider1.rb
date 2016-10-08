93c149601ea126e559c5053afaeb0fc9f5bb5be2
Varun Yellina <varun@yellina.io>
2016-10-08 05:52:00 +0530

    Added MacHider

---
+++ Casks/machider1.rb
@@ -0,0 +1,11 @@
+cask 'fantastical1' do
+  version '1.6.7'
+  sha256 'a060a91ebdcbce09a0418319e6c33a7e8586772ba2014b94b6825b9452a39455'
+
+  url "http://dl.devmate.com/com.macpaw.MacHider/MacHider.dmg"
+  name 'MacHider'
+  homepage 'http://macpaw.com/support/hider/knowledgebase/machider-support'
+  license :freemium
+
+  app 'MacHider.app'
+end
